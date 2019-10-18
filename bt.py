b5c81c9646231fc0e9594a7cd4c5fe8311d22d02
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-18 22:31:28 +0530

    Update bt.py

--- bt.py
+++ bt.py
@@ -3,32 +3,6 @@ from pyrogram.errors import FloodWait
 import time
 app = Client("session",bot_token="790494538:AAHb1mX0CI31eIl3nSpCAoY92bmYeLYdBSM",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
 k = -1001289914295
-@app.on_message(Filters.command("clr"))
-def forward(client, message):
-  with open("ids.txt" , "w") as file:
-   file.write("0001")
-   file.close() 
-   message.reply("kk")
-@app.on_message(Filters.command("set") & Filters.user("705138975"))
-def forward(client, message):
-  x = message.text.split(" ")
-  if x.casefold() == "ferrari".casefold():
-   with open("source.txt" , "w") as file:
-    file.write("-1001452956784")
-    file.close()
-    message.reply("☢️ Done, Line has been changed to** " + x + " **✅✅")
-  elif x.casefold() == "bullet".casefold():
-   with open("source.txt" , "w") as file:
-    file.write("-1001152344101")
-    file.close()
-    message.reply("☢️ Done, Line has been changed to** " + x + " **✅✅")
-  elif x.casefold() == "live".casefold():
-   with open("source.txt" , "w") as file:
-    file.write("-1001468447534")
-    file.close()
-    message.reply("☢️ Done, Line has been changed to** " + x + " **✅✅")
-  else:
-   Message.reply("**Line not exist !!**")
 @app.on_message( Filters.text & ~Filters.edited)
 def forward(client, message):
  fil = open("source.txt" , "r")
@@ -58,4 +32,30 @@ def forward(client, message):
       client.edit_message_text(k,int(x[x.index(id)+1]),message.text)
      except FloodWait as e:
       time.sleep(e.x)
+@app.on_message(Filters.command("set") & Filters.user(705138975))
+def forward(client, message):
+  x = message.text.split(" ")
+  if x.casefold() == "ferrari".casefold():
+   with open("source.txt" , "w") as file:
+    file.write("-1001452956784")
+    file.close()
+    message.reply("☢️ Done, Line has been changed to** " + x + " **✅✅")
+  elif x.casefold() == "bullet".casefold():
+   with open("source.txt" , "w") as file:
+    file.write("-1001152344101")
+    file.close()
+    message.reply("☢️ Done, Line has been changed to** " + x + " **✅✅")
+  elif x.casefold() == "live".casefold():
+   with open("source.txt" , "w") as file:
+    file.write("-1001468447534")
+    file.close()
+    message.reply("☢️ Done, Line has been changed to** " + x + " **✅✅")
+  else:
+   Message.reply("**Line not exist !!**")
+@app.on_message(Filters.command("Clear") &Filters.user(705138975))
+def forward(client, message):
+  with open("ids.txt" , "w") as file:
+   file.write("0001")
+   file.close() 
+   message.reply("Done,editing Data cleared !!")
 app.run()
