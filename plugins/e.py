45b620fa2a6a70164fd7434ce86c8af2d37e4ea6
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2020-09-16 03:17:20 -0700

    Create e.py

---
+++ plugins/e.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import time
+
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import time@app.on_message(filters.command('clear') & filters.user(491634139))
+async def forward(client, message):
+    file = open(message.text.split(" ")[1] + ".txt" , "r")
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+     p = line.split()
+     for x in p:
+        fie = open(str(x)+".txt","w")
+        fie.write("001 002")
+        fie.close()
+        await message.reply("☢️ Done, Editing data cleared ✅✅")
