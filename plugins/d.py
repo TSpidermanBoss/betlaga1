e344d9d1960d126d4132ab9f124b8f596f8537f2
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2020-09-16 03:15:38 -0700

    Create d.py

---
+++ plugins/d.py
@@ -0,0 +1,19 @@
+@app.on_message(filters.command('remove') & filters.user(491634139))
+async def forward(client, message):
+ if len(message.text.split(' ')) > 2:
+  if len(message.text.split(' ')[1]) == 14:
+   file = open(message.text.split(" ")[2] + ".txt" , "r")
+   u = file.readlines()
+   file.close()
+   for v in u:
+     lines = v.split() 
+     del lines[lines.index(message.text.split(' ')[1])]
+     y = " ".join(str(x) for x in lines)
+     files = open(message.text.split(" ")[2] + ".txt" , "w") 
+     files.write(y)
+     files.close()
+     await message.reply("💾 Done, The chat_id  ```" + message.text.split(' ')[1] +"```🌐 has been removed to my database. ✅✅")
+  else:
+    await message.reply("💼 Please write a valid chat id. ✅✅ ")
+ else:
+    await message.reply("💼 Please write a valid chat id. ✅✅ ")
