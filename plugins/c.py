e7702397f631b706c2dd0fdfee634d36c3c81505
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2020-09-16 02:14:47 -0700

    Update c.py

--- plugins/c.py
+++ plugins/c.py
@@ -1,1 +1,95 @@
-cc
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import time
+@app.on_message(filters.command('add') & filters.user(491634139) )
+def forward(client, message):
+ if len(message.text.split(' ')) > 2:
+  if len(message.text.split(' ')[1]) == 14:
+   with open(message.text.split(" ")[2] + ".txt" , "r") as file:
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+     files = open(message.text.split(" ")[2] + ".txt" , "w") 
+     files.write(line + " " + message.text.split(' ')[1])
+     files.close()
+     with open(message.text.split(' ')[1]+".txt" , "w") as g:
+       g.write("001 002")
+       g.close()
+     message.reply("💾 Done, The chat_id  ```" + message.text.split(' ')[1] +"```🌐 has been added to my database. ✅✅")
+  else:
+    message.reply("💼 Please write a valid chat id. ✅✅ ")
+ else:
+   message.reply("💼 Please write a valid chat id. ✅✅ ")
+@app.on_message(Filters.command('remove') & Filters.user(491634139))
+def forward(client, message):
+ if len(message.text.split(' ')) > 2:
+  if len(message.text.split(' ')[1]) == 14:
+   file = open(message.text.split(" ")[2] + ".txt" , "r")
+   u = file.readlines()
+   file.close()
+   for v in u:
+     lines = v.split() 
+     del lines[lines.index(message.text.split(' ')[1])]
+     y = " ".join(str(x) for x in lines)
+     files = open(message.text.split(" ")[2] + ".txt" , "w") 
+     files.write(y)
+     files.close()
+     message.reply("💾 Done, The chat_id  ```" + message.text.split(' ')[1] +"```🌐 has been removed to my database. ✅✅")
+  else:
+    message.reply("💼 Please write a valid chat id. ✅✅ ")
+ else:
+    message.reply("💼 Please write a valid chat id. ✅✅ ")
+@app.on_message(Filters.command('clear') & Filters.user(491634139))
+def forward(client, message):
+    file = open(message.text.split(" ")[1] + ".txt" , "r")
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+     p = line.split()
+     for x in p:
+        fie = open(str(x)+".txt","w")
+        fie.write("001 002")
+        fie.close()
+        message.reply("☢️ Done, Editing data cleared ✅✅")
+@app.on_message(Filters.command('list') & Filters.user(491634139))
+def forward(client, message):
+  file = open(message.text.split(" ")[1] + ".txt" , "r")
+  u = file.readlines()
+  file.close()
+  for v in u :
+      message.reply("🏘️ List of Chat_ids in my database are ```" + str(v) + "```. Its can be change. ✅✅")
+@app.on_message(Filters.command('get') & Filters.user(491634139) )
+def forward(client, message):
+ if len(message.text.split(' ')) > 1:
+  if len(message.text.split(' ')[1]) == 14:
+      x = client.get_chat(int(message.text.split(' ')[1])).title
+      message.reply("📶 This chat name is - "+str(x)+" ✅")
+  else:
+    message.reply("💼 Please write a valid chat id. ✅✅ ")
+ else:
+    message.reply("💼 Please write a valid chat id. ✅✅ ")
+
+@app.on_message(Filters.command('reset') & Filters.user(491634139))
+def forward(client, message):
+ with open("ferrari.txt" , "w") as g:
+  g.write("-1001353340635")
+  g.close()
+ with open("bullet.txt" , "w") as g:
+  g.write("-1001353340635")
+  g.close()
+ with open("ids.txt" , "w") as g:
+  g.write("-1001353340635")
+  g.close()
+ message.reply("done")
+
+
+@app.on_message(Filters.command("start"))
+def forward(client, message):
+ if message.from_user.id == 491634139:
+   message.reply("♻️ Welcome to your LineBot . ✅✅")
+ else:
+   message.reply("♻️ You need admins permission to use my functions. ✅✅")
+@app.on_message(Filters.private)
+def forward(client, message):
+ if not message.from_user.id == 491634139:
+   message.reply("♻️ You need admins permission to use my functions. ✅✅")
