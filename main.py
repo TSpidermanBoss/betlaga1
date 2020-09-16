099d6f112ffeacd3c5b2bb28d72351636dccbc1f
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2020-09-16 02:22:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from pyrogram import Client
 
-Client("my_account").run()
+Client("mnnn",bot_token="918978080:AAHHAghRQq83v3tDhQp1H75rvbPpjHAgmZ0",api_id=768402,api_hash="f6420bf67303614279049d48d3e670f6").run()
